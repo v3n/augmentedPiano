cc2c5e51590a3020e3915551b2fd0ac96cdb970e
Jonathan Howard <j@hovverd.com>
2014-04-15 17:53:49 -0400

    Improved contour detection
    Now performs morphological closing to select entire keyboard. Additionally,
    selects only the largest contour (which should be the keyboard).

--- augmentedPiano/APViewController.mm
+++ augmentedPiano/APViewController.mm
@@ -57,7 +57,8 @@ Mat dst, detected_edges;
 {
     // Do some OpenCV stuff with the image
     Mat image_copy;
-    cvtColor(image, image_copy, CV_BGR2GRAY);
+    Mat grey_copy;
+    cvtColor(image, image_copy, CV_BGR2BGRA);
     
     /// Reduce noise with a kernel 3x3
     GaussianBlur(image_copy, detected_edges, cv::Size(3,3), 2, 2);
@@ -70,7 +71,38 @@ Mat dst, detected_edges;
     
     image.copyTo( dst, detected_edges);
     
-    cvtColor(dst, image, CV_BGR2BGRA);
+    //Contours
+    cv::cvtColor(image, grey_copy, CV_BGR2GRAY);
+//    threshold(grey_copy, grey_copy, 80, 255, CV_THRESH_BINARY);
+    threshold(grey_copy, grey_copy, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+    
+    //morphological close to remove lines between keys (we want entire keyboard)
+    morphologyEx(grey_copy, grey_copy,
+                 MORPH_CLOSE,
+                 getStructuringElement(MORPH_RECT, cv::Size(8,5)));
+//    image = grey_copy.clone();
+
+    
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<cv::Vec4i> hierarchy;
+    int keyboardIdx = 0;
+    double prevArea = 0.0;
+    
+    cv::findContours( grey_copy, contours, hierarchy, cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE);
+    for ( size_t i=0; i<contours.size(); ++i )
+    {
+        cv::Rect brect = cv::boundingRect(contours[i]);
+        if(brect.area() > prevArea) {
+            keyboardIdx = (int)i;
+            prevArea = brect.area();
+        }
+    }
+    
+    cv::drawContours( image, contours, keyboardIdx, Scalar(200,0,0), 1, 8, hierarchy, 0, cv::Point() );
+//    cv::rectangle(image, brect, Scalar(255,0,0));
+
+//    cv::cvtColor(image, image, CV_BGR2GRAY);
+//    cvtColor(dst, image, CV_BGR2BGRA);
 }
 #endif
 
